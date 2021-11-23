11d16f66d958946c630df0d0099c7a9f0a280320
Donbass <ladasfeed@mail.ru>
2021-11-23 16:00:55 +0300

    +

--- src/Components/index.tsx
+++ src/Components/index.tsx
@@ -1,11 +1,9 @@
 import InputTextBuilder from "./InputBuilder";
 import { SwitchBuilder } from "./Switch";
-// import { CalendarBuilder } from "./CalendarCore";
 import { CheckInputBuilder } from "./CheckInputCore";
 
 export const UIBuilders = {
   InputTextBuilder: InputTextBuilder.InputTextBuilder,
   SwitchBuilder,
   CheckInputBuilder,
-  // CalendarBuilder,
 };
