d93fce9323772627eff46924a11c7e519cb06c8b
Donbass <ladasfeed@mail.ru>
2021-11-26 09:38:12 +0300

    api fix

--- src/Builders/api/index.ts
+++ src/Builders/api/index.ts
@@ -1,13 +1,16 @@
 import { AxiosResponse } from "axios";
 
-export function apiBuilder<T, F>(
-  method: (props: T) => Promise<F>
-): (apiProps: T) => Promise<F> {
-  return async (apiProps: T) => {
+export function apiBuilder<T>(
+  method: (props: T) => Promise<AxiosResponse<any>>
+): (apiProps: T) => Promise<AxiosResponse<any>> {
+  const func: (apiProps: T) => Promise<AxiosResponse<any>> = async (
+    apiProps: T
+  ) => {
     try {
       return await method(apiProps);
     } catch (e: any) {
       throw await e.response;
     }
   };
+  return func;
 }
