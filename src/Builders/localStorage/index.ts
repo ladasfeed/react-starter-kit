e8c9e46e725202de764cae786b11c4123991f05c
Donbass <ladasfeed@mail.ru>
2022-01-28 12:30:07 +0300

    ls fixbug

--- src/Builders/localStorage/index.ts
+++ src/Builders/localStorage/index.ts
@@ -1,32 +1,37 @@
 const rawLc = window.localStorage;
 export function localStorageBuilder<storageType>() {
-   return {
-      get: function <T extends keyof storageType>(key: T) {
-         const item = rawLc.getItem(String(key));
-         if (item) {
-            return JSON.parse(item) as storageType[T];
-         } else {
-            return null;
-         }
-      },
-      set: function <T extends keyof storageType>(
-         key: T,
-         value: storageType[T]
-      ) {
-         switch (typeof value) {
-            case "string":
-            case "number":
-            case "boolean":
-            case "object":
-               rawLc.setItem(String(key), JSON.stringify(value));
+  return {
+    get: function <T extends keyof storageType>(key: T) {
+      const item = rawLc.getItem(String(key));
+      let parsedValue: storageType[T];
 
-               break;
-            default:
-               return;
-         }
-      },
-      clearAll: function () {
-         rawLc.clear();
-      },
-   };
+      try {
+        if (item) {
+          parsedValue = JSON.parse(item);
+
+          return parsedValue;
+        } else {
+          return null;
+        }
+      } catch (e) {
+        return item;
+      }
+    },
+    set: function <T extends keyof storageType>(key: T, value: storageType[T]) {
+      switch (typeof value) {
+        case "string":
+        case "number":
+        case "boolean":
+        case "object":
+          rawLc.setItem(String(key), JSON.stringify(value));
+
+          break;
+        default:
+          return;
+      }
+    },
+    clearAll: function () {
+      rawLc.clear();
+    },
+  };
 }
